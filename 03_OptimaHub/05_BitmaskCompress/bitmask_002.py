4770cd7381a889ba883459a1ee8c1ba4af36335e
cherry_77 <1615405@qq.com>
2024-09-13 12:43:06 +0800

    Update bitmask_002.py

--- 03_OptimaHub/05_BitmaskCompress/bitmask_002.py
+++ 03_OptimaHub/05_BitmaskCompress/bitmask_002.py
@@ -22,3 +22,23 @@ class Solution:
             return max_score
         
         return dfs(0, 0)
+
+
+
+class Solution:
+    def maxCompatibilitySum(self, students: List[List[int]], mentors: List[List[int]]) -> int:
+        m, n = len(students), len(students[0])
+        g = [[0] * m for _ in range(m)]
+
+        for i in range(m):
+            for j in range(m):
+                g[i][j] += sum(students[i][k] == mentors[j][k] for k in range(n))
+
+        f = [0] * (1 << m)
+        for mask in range(1, 1 << m):
+            c = bin(mask).count("1")
+            for i in range(m):
+                if mask & (1 << i):
+                    f[mask] = max(f[mask], f[mask ^ (1 << i)] + g[c - 1][i])
+        
+        return f[(1 << m) - 1]
