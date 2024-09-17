d97b361286d0efac8c256111e048265e3f424a73
cherry_77 <1615405@qq.com>
2024-09-17 21:18:34 +0800

    Create segment_004.py

---
+++ 01_EssentialThicket/04_DeltaSummation/segment_004.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numberOfPoints(self, nums: List[List[int]]) -> int:
+        max_end = max(end for _, end in nums)
+        diff = [0] * (max_end + 2)
+        for start, end in nums:
+            diff[start] += 1
+            diff[end + 1] -= 1
+        return sum(s > 0 for s in accumulate(diff))
